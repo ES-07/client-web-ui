9d0487c4b4dad4cb2aeebd1661bff0ecb9ad7f14
reis <joaoreis16@ua.pt>
2022-12-17 00:59:15 +0000

    link do loadbalancer

--- client-ui/src/environments/environment.prod.ts
+++ client-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SITES_API: 'http://localhost:8002',
+  SITES_API: 'http://alb-643791497.eu-west-3.elb.amazonaws.com/sites-management-api/',
   cognito: {
     region : 'eu-west-3',
     userPoolId : 'eu-west-3_jf78B1lET',
