9e6c814425c11af8451be04e81dcfbba7f79bdf7
reis <joaoreis16@ua.pt>
2022-12-18 01:07:20 +0000

    não mexo mais em .env files

--- client-ui/src/environments/environment.ts
+++ client-ui/src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  SITES_API: "",
+  SITES_API: 'http://alb-643791497.eu-west-3.elb.amazonaws.com/sites-management-api',
   cognito: {
-    region : "",
-    userPoolId : '',
-    userPoolWebClientId : '',
+    region : 'eu-west-3',
+    userPoolId : 'eu-west-3_jf78B1lET',
+    userPoolWebClientId : '40jrepbggimcpd1tcigpp045ui',
     oauth: {
-      domain: '',
+      domain: 'arn:aws:cognito-idp:eu-west-3:096876465810:userpool/eu-west-3_jf78B1lET',
       scope: ['openid'],
       redirectSignIn:  'http://localhost:4200/',
       redirectSignOut: 'http://localhost:4200/',
@@ -20,10 +20,12 @@ export const environment = {
       }
     }
   },
-  accessKeyId: '',
-  secretAccessKey: '',
+  accessKeyId: 'AKIARNDSJR2JHDUDT7KB',
+  secretAccessKey: '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',
+  region : 'eu-west-3',
 
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
